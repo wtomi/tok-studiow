39c864b867c20712e31339f1291d410f4eea126c
Juliusz Jezierski <Juliusz.Jezierski@cs.put.poznan.pl>
2017-12-02 16:01:26 +0100

    01-init

---
+++ src/main/java/pl/dziekanat/tokstudiow/ZaliczenieSemestru.java
@@ -0,0 +1,9 @@
+package pl.dziekanat.tokstudiow;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Loan Approval App")
+public class ZaliczenieSemestru  extends ServletProcessApplication{
+
+}

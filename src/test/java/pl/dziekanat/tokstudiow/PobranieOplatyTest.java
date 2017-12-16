123ef6fa91f07702491ba42101549e90cf0d73bf
Juliusz Jezierski <Juliusz.Jezierski@cs.put.poznan.pl>
2017-12-16 13:52:57 +0100

    resolving unlimited loop problem

--- src/test/java/pl/dziekanat/tokstudiow/PobranieOplatyTest.java
+++ src/test/java/pl/dziekanat/tokstudiow/PobranieOplatyTest.java
@@ -20,7 +20,7 @@ import org.camunda.bpm.engine.HistoryService;
 import org.camunda.bpm.engine.ProcessEngineConfiguration;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
 
 import java.util.List;
 
@@ -32,29 +32,56 @@ public class PobranieOplatyTest {
 
 	  @Test
 	  @Deployment(resources = {"PobranieOplaty.bpmn"})
-	  public void ruleUsageExample() {
+	  public void testPobranieOplaty() {
 	    RuntimeService runtimeService = processEngineRule.getRuntimeService();
 	    HistoryService historyService = processEngineRule.getHistoryService();
 	    VariableMap variablesIn = Variables.createVariables()
-	  	      .putValue("oplata_kwota", -11);
+	  	      .putValue("oplata_kwota", 100);
 	    ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("PobranieOplaty", variablesIn);
-
-//	    TaskService taskService = processEngineRule.getTaskService();
-//	    Task task = taskService.createTaskQuery().singleResult();
-//	    assertEquals("My Task", task.getName());
-
-//	    taskService.complete(task.getId());
-	    assertEquals(0, runtimeService.createProcessInstanceQuery().count());
 	    
-//	    assertProcessEnded(processInstance.getId());
-/*
+	    assertThat(processInstance).isEnded();
+	    
 	     List<HistoricVariableInstance> oplata_nrTransakcjiList = historyService.createHistoricVariableInstanceQuery().variableName("oplata_nrTransakcji").list();
 	     HistoricVariableInstance oplata_nrTransakcji = oplata_nrTransakcjiList.get(oplata_nrTransakcjiList.size()-1);
 	     assertEquals("ABC321", oplata_nrTransakcji.getValue());
-*/	    
-
-//	    if (isFullHistoryEnabled()) {
-//	      assertEquals(3, historyService.createHistoricDetailQuery().count());
-//	    }
+	    
 	  }
+
+
+	  @Test
+	  @Deployment(resources = {"PobranieOplaty.bpmn", "OcenaPodania.dmn", "ZaliczenieSemestru.bpmn"})
+	  public void testZaliczenieSemestru() {
+	    RuntimeService runtimeService = processEngineRule.getRuntimeService();
+	    VariableMap variablesIn = Variables.createVariables()
+	    	  .putValue("podanie_nrAlbumu", "29000")
+	    	  .putValue("podanie_punktyECTS", 15)
+	    	  .putValue("podanie_uzasadnienie", "kro'tkie")	    	  
+	    	  .putValue("czyJestDecyzja", false)
+	  	      .putValue("oplata_status", "rozpoczeta")
+	  	      
+	  	      ;
+	    ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("ZaliczenieSemestru", variablesIn);
+	    
+	    assertThat(processInstance).isWaitingAt("DecyzjaDziekanatu");
+	    complete(task(processInstance), withVariables("decyzja_czyPozytywna", true));
+	    
+	    assertThat(processInstance).isWaitingAt("WprowadzenieDanychPlatnosci");
+	    complete(task(processInstance), withVariables("oplata_kwota", -11, "oplata_czyZgoda", true));
+	    
+	    assertThat(processInstance).isWaitingAt("WprowadzenieDanychPlatnosci");
+	    assertThat(processInstance).variables().containsEntry("oplata_status", "OplataKwotaErrCode");
+	    complete(task(processInstance), withVariables("oplata_kwota", 100, "oplata_czyZgoda", true));      
+	    
+	    assertThat(processInstance).isWaitingAt("OdbiorDecyzji");
+	    assertThat(processInstance).variables().containsEntry("decyzja_czyPozytywna", true);
+	    assertThat(processInstance).variables().containsEntry("oplata_kwota", 100);
+	    assertThat(processInstance).variables().containsEntry("oplata_nrTransakcji", "ABC321");
+	    assertThat(processInstance).variables().containsEntry("oplata_status", "ABC321");
+	    complete(task(processInstance));
+	    
+	    assertThat(processInstance).isEnded();
+	    
+	    
+	  }	  
+	  
 	}
